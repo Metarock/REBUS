93b688b200a3be0745a78c6d03735fc17323a28d
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-09-24 02:39:22 +1200

    Added Enemy Health, and Health Bar
    Added EnemyHealthManager and EnemyHealthDamager script for adding health to enemy and allowing the enemy to be damaged.
    Also added a health bar to show the current health of the player.

---
+++ Rebus/Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider healthBar;
+    public PlayerHealthManager playerHealth;
+
+    public static bool UIExists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!UIExists)
+        {
+            UIExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.maxValue = playerHealth.playerMaxHealth;
+        healthBar.value = playerHealth.playerCurrentHealth;
+    }
+
+
+}

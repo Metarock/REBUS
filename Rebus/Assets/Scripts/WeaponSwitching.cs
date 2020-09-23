b8c96b8e2404cdc78bc3033b3b0a726630dbdc02
JemRC <69658441+JemRC@users.noreply.github.com>
2020-09-24 11:07:39 +1200

    UZI Player Changes
    Changed Box Collider size, and changed texture from bilinear to no point

--- Rebus/Assets/Scripts/WeaponSwitching.cs
+++ Rebus/Assets/Scripts/WeaponSwitching.cs
@@ -53,6 +53,27 @@ public class WeaponSwitching : MonoBehaviour
             }
         }
 
+        // Number Keys
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+
         if (previousSelectedWeapon != selectedWeapon)
         {
             SelectWeapon();

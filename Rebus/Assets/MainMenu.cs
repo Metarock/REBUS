50f734bb5e002a821448850a93a88106ef550862
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-09-24 17:53:43 +1200

    Improved Main Menu
    Improved main menu. Comes with button functionality (only for the play button and quit button), and better button icons.

---
+++ Rebus/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Function to start game (for PLAY button)
+    public void PressPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Function to quit game (for QUIT button)
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

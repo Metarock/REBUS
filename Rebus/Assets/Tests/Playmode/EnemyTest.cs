27a212a8dc912876a5e36cc0b07e2069bb30a3b5
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-09-26 00:34:45 +1200

    Fixed Enemy, Added Enemy Prefab
    Fixed the enemy prefab as it had no components before.

--- Rebus/Assets/Tests/Playmode/EnemyTest.cs
+++ Rebus/Assets/Tests/Playmode/EnemyTest.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
     public class EnemyTest
     {
-        [Test]
-        public void TestShootingAI()
+        [UnityTest]
+        public IEnumerator EnemyShootingTest()
         {
+            SceneManager.LoadScene("MainMenu");
+            yield return new WaitForSeconds(10);
+
             //Assign
-
-
-            var gameObject = new GameObject();
+            var gameObject = new GameObject("enemy");
             gameObject.AddComponent<Enemy>();
+            gameObject.AddComponent<Animator>();
+            gameObject.AddComponent<Rigidbody2D>();
+            //gameObject.AddComponent<BoxCollider2D>();
             Enemy enemy = gameObject.GetComponent<Enemy>();
-            bool statementHolds = false;
             
             enemy.GetComponent<Enemy>().target.transform.position = new Vector3(5, 5, 10);
             enemy.GetComponent<Enemy>().transform.position = new Vector3(5, 5, 2);
@@ -26,24 +30,11 @@ namespace Tests
 
             Quaternion correctRotation = new Quaternion(-90, 0, 0, 1);
 
-
-
             //Act
             enemy.ShootingAI();
 
-            //if(enemy.GetComponent<Enemy>().)
-            
-
-            /*enemy.isExploded = false;
-            enemy.dirX = 1f;
-            enemy.dirY = 2f;
-
-
-            enemy.FixedUpdate();
-
-            Assert.AreEqual(expected true, actual )*/
-
-
+            //Assert
+            Assert.AreEqual(correctRotation, enemy.GetComponent<Enemy>().transform.rotation);
         }
     }
 }

334ad69a6af11d2056b508530873e4076b5e0ada
ycy190 <ycy190@163.com>
2020-10-15 18:16:27 +1300

    v1.0 pause menu
    Workload: Office basement floor pause menu implemented(able to pause resume or jump back to main menu)
    but pause menu only works on basement floor, not working on second floor or third floor(although pressing esc can opt out pause menu correctly...

--- Rebus/Assets/Scripts/UIManager.cs
+++ Rebus/Assets/Scripts/UIManager.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
 
 public class UIManager : MonoBehaviour
 {
+    public static bool paused = false;
+
+    public GameObject pauseUI;
     public Slider healthBar;
     public PlayerHealthManager playerHealth;
 
     public static bool UIExists;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        if(!UIExists)
+        if (!UIExists)
         {
             UIExists = true;
+            pauseUI.SetActive(false);
             DontDestroyOnLoad(transform.gameObject);
         }
         else
@@ -25,9 +32,48 @@ public class UIManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
         healthBar.maxValue = playerHealth.playerMaxHealth;
         healthBar.value = playerHealth.playerCurrentHealth;
     }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseUI.SetActive(false);
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseUI.SetActive(true);
+        paused = true;
+
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        //not implemented due to not required
+        //Application.Quit();
+    }
+
+
 }

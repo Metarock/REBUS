c39d78a0ada5db8850128c631d583f632755f240
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-10-21 15:11:34 +1300

    Eliminated Message Added
    Finally fixed the issue where the Message doesn't appear.
    
    Co-Authored-By: JemRC <69658441+JemRC@users.noreply.github.com>

--- Rebus/Assets/Scripts/UIManager.cs
+++ Rebus/Assets/Scripts/UIManager.cs
@@ -7,16 +7,22 @@ public class UIManager : MonoBehaviour
 {
     public Slider healthBar;
     public PlayerHealthManager playerHealth;
+    public GameObject eliminatedMessage;
+    private int enemyCount;
 
     public static bool UIExists;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(!UIExists)
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (!UIExists)
         {
             UIExists = true;
             DontDestroyOnLoad(transform.gameObject);
+            Debug.Log("Start!");
+            eliminatedMessage.SetActive(false);
         }
         else
         {
@@ -29,5 +35,19 @@ public class UIManager : MonoBehaviour
     {
         healthBar.maxValue = playerHealth.playerMaxHealth;
         healthBar.value = playerHealth.playerCurrentHealth;
+
+        // Checks the amount of enemies present in the scene
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        Debug.Log("Message: " + enemyCount);
+
+        if (enemyCount <= 0)
+        {
+            eliminatedMessage.SetActive(true);
+            Debug.Log("True!");
+        }
+        else
+        {
+            eliminatedMessage.SetActive(false);
+        }
     }
 }

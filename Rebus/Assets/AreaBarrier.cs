b3bc67c2973bc233abb1d3db760a36de6b5b03ce
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-10-21 14:56:26 +1300

    Barriers Added
    Added barriers for the house level. Currently working on displaying a message to the user when they have eliminated all enemies in that area.
    
    Co-Authored-By: JemRC <69658441+JemRC@users.noreply.github.com>

---
+++ Rebus/Assets/AreaBarrier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaBarrier : MonoBehaviour
+{
+    private int enemyCount;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Checks the amount of enemies present in the scene. At the start, it records it.
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks the amount of enemies present in the scene
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount <= 0)
+        {
+            // If there are no more enemies, the barrier is disabled.
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            this.spriteRenderer.enabled = false;
+        }
+        else
+        {
+            // Barrier stays enabled as long as there are enemies.
+            this.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}

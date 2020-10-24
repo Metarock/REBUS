e800030959108329ef94fd4f0d8d5af4db6dea50
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-10-24 19:44:37 +1300

    Start on Level Unlock System
    Started on the level unlock system. Added some scripts to the main menu for disabling levels the player has to unlock. Will have to add more scripts in the future to make it completely work.

---
+++ Rebus/Assets/LevelController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public Button houseLevel;
+    public Button officeLevel;
+    public Button laboratoryLevel;
+
+    int levelPassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+
+        officeLevel.interactable = false;
+        laboratoryLevel.interactable = false;
+
+        switch (levelPassed)
+        {
+            case 1:
+                officeLevel.interactable = true;
+                break;
+            case 2:
+                officeLevel.interactable = true;
+                laboratoryLevel.interactable = true;
+                break;
+        }
+    }
+}

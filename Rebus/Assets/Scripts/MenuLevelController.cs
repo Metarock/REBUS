85c62388d32a786a4dc4d125369bd5d22b14e3fc
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-10-25 00:48:57 +1300

    Level Unlock System Improvements
    Added various features:
    - Player can now reset their progress in the Options menu
    - Player can now progress to level 2 (transition from level 1 to level 2 is still in development)
    
    Note that there are various bugs to fix when moving between levels. (MAY HAVE TO HAVE ONLY ONE SINGLE PLAYER, CAMERA AND UI IN THE WHOLE GAME)

--- Rebus/Assets/Scripts/MenuLevelController.cs
+++ Rebus/Assets/Scripts/MenuLevelController.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class LevelController : MonoBehaviour
+public class MenuLevelController : MonoBehaviour
 {
     public Button houseLevel;
     public Button officeLevel;
@@ -13,6 +13,7 @@ public class LevelController : MonoBehaviour
     int levelPassed;
 
     // Start is called before the first frame update
+    //Checks playerprefs to see which levels are to be unlocked (all levels - except hosue level - are disabled/locked by default)
     void Start()
     {
         levelPassed = PlayerPrefs.GetInt("LevelPassed");
@@ -30,5 +31,15 @@ public class LevelController : MonoBehaviour
                 laboratoryLevel.interactable = true;
                 break;
         }
+    }
+
+    //Reset player progress. This resets all levels and will lock all currently unlocked levels.
+    public void ResetProgress()
+    {
+        PlayerPrefs.SetInt("LevelPassed", 0);
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+
+        officeLevel.interactable = false;
+        laboratoryLevel.interactable = false;
     }
 }

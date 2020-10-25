9d11f9a0cb21e68ac3dfa7e365de65abebcc10d5
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2020-10-25 17:11:39 +1300

    Transition from House to Office
    Made improvements to the level unlock system, however there are still various bugs to fix.

---
+++ Rebus/Assets/LevelStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStart : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    void ResetPlayer()
+    {
+        player.GetComponent<PlayerHealthManager>().RestoreCurrentHealth();
+    }
+}
--- Rebus/Assets/Scripts/MessageIndicator.cs
+++ Rebus/Assets/Scripts/MessageIndicator.cs
@@ -25,6 +25,5 @@ public class MessageIndicator : MonoBehaviour
         {
             this.GetComponent<Text>().enabled = true;
         }
-
     }
 }
